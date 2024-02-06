44eb36c6da8e2dc2f4558d0f6f48a494129d919a
LazizbekDev <lazizbektojiboyev0611@gmail.com>
2024-02-06 11:06:44 +0500

    linked to instagram

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -61,7 +61,7 @@ export default function Popup({open, close, square}) {
                                     </div>
                                 </div>
                                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                    <a href={"tel:+998770003232"}>
+                                    <a target={"_blank"} rel={"noreferrer"} href={"https://instagram.com/tehnoplast.uz"}>
                                         <button
                                             type="button"
                                             className="inline-flex w-full justify-center rounded-md bg-[#DE5801] px-3 py-2 text-sm font-semibold text-black shadow-sm hover:bg-yellow-500 sm:ml-3 sm:w-auto"
